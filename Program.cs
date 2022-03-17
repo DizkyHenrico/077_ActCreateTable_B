5e226ae9316fac475d4c7356759cc875a1b080d7
DizkyHenrico <henricozdzky@gmail.com>
2022-03-17 15:21:25 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CreateTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Create();
+        }
+
+        public void Create()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source = DIZKY\\DIZKY ;database=ProdiTI ;Integrated Security = True");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("Create Table Mahasiswa_Coba (NIM Char(12) not null primary key, Nama Varchar(50), Alamat Varchar(50), Jenis_Kelamin char(5))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Create tabel berhasil");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error",  e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
